9c8d5d95b68fd85188466a665fb74d9fca5f9849
Abhijit190590 <abhijitdehury1905@gmail.com>
2025-05-22 10:12:36 +0530

    Zidio backend

---
+++ ZidioProject_Backend/src/main/java/com/example/ZidioProject_Backend/ZidioProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZidioProject_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZidioProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZidioProjectBackendApplication.class, args);
+	}
+
+}
---
+++ ZidioProject_Backend/src/test/java/com/example/ZidioProject_Backend/ZidioProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZidioProject_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZidioProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
