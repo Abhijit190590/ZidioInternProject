87591aee5c7ba19333c1fba40e994e4a41ae51eb
Abhijit190590 <abhijitdehury1905@gmail.com>
2025-06-26 09:50:46 +0530

    new backand

---
+++ zidioBackEP/src/main/java/com/example/Zproject/ZidioBackEpApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZidioBackEpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZidioBackEpApplication.class, args);
+	}
+
+}
---
+++ zidioBackEP/src/test/java/com/example/Zproject/ZidioBackEpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZidioBackEpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
